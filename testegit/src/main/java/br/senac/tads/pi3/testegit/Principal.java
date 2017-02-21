1399669477e461096cad06b157d7648e4e44045d
aline.mdmorato <aline.mdmorato@CAS511749W0723.senacsp.edu.br>
2017-02-21 20:38:26 -0300

    Mostrar 10 vezes

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -15,9 +15,10 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.println("Hello World!!");
-        
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World " + i + " !!");
+        }
     }
-    
+
 }
