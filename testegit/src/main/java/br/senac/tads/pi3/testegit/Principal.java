e1c14c6ed39fee4a6e13deb03454472f249f9344
alinemorato <alinemorato@192.168.0.12>
2017-03-06 22:12:16 -0300

    Nome Data de nascimento Telefone E-mail * falta "as informações referentes ao ID e a data e hora atual devem ser incluídas" *

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -5,22 +5,173 @@
  */
 package br.senac.tads.pi3.testegit;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author aline.mdmorato
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
 
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println("Hello World " + i + " !!");
+        // Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", // usuario
+                "app"); // senha
+        return conn;
+    }
+
+    public void listarPessoas() {
+        Statement stmt = null;
+        Connection conn = null;
+
+        String sql = "SELECT ID_PESSOA, NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL FROM TB_PESSOA";
+        try {
+            conn = obterConexao();
+            stmt = conn.createStatement();
+            ResultSet resultados = stmt.executeQuery(sql);
+
+            DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+
+            while (resultados.next()) {
+                Long id = resultados.getLong("ID_PESSOA");
+                String nome = resultados.getString("NM_PESSOA");
+                Date dataNasc = resultados.getDate("DT_NASCIMENTO");
+                String email = resultados.getString("VL_EMAIL");
+                String telefone = resultados.getString("VL_TELEFONE");
+                System.out.println(String.valueOf(id) + ", " + nome + ", " + formatadorData.format(dataNasc) + ", " + email + ", " + telefone);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
 
+    public void incluirPessoa() {
+        PreparedStatement stmt = null;
+        Connection conn = null;
+
+        String nome;
+        java.util.Date dataNasc;
+        String email;
+        String telefone;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o nome da pessoa: ");
+        nome = entrada.nextLine();
+
+        System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
+        String strDataNasc = entrada.nextLine();
+        DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            dataNasc = formatadorData.parse(strDataNasc);
+        } catch (ParseException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+            dataNasc =new java.util.Date();
+        }
+        
+        
+        System.out.print("Digite o telefone no formato 99 99999-9999: ");
+        telefone = entrada.nextLine();
+
+        System.out.print("Digite o e-mail: ");
+        email = entrada.nextLine();
+
+        String sql = "INSERT INTO TB_PESSOA (NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL) VALUES (?, ?, ?, ?)";
+        try {
+            conn = obterConexao();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, nome);
+            stmt.setDate(2, new java.sql.Date(dataNasc.getTime()));
+            stmt.setString(3, telefone);
+            stmt.setString(4, email);
+            stmt.setDate(5, new java.sql.Date(System.currentTimeMillis()));
+            stmt.executeUpdate();
+            System.out.println("Registro incluido com sucesso.");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Principal instancia = new Principal();
+        Scanner entrada = new Scanner(System.in);
+        do {
+            System.out.println("********** DIGITE UMA OPÇÃO **********");
+            System.out.println("(1) Listar agenda");
+            System.out.println("(2) Incluir registro");
+            System.out.println("(9) SAIR");
+            System.out.print("Opção: ");
+            int opcao = entrada.nextInt();
+
+            if (opcao == 1) {
+                instancia.listarPessoas();
+            } else if (opcao == 2) {
+                instancia.incluirPessoa();
+            } else if (opcao == 9) {
+                System.exit(0);
+            } else {
+                System.out.println("OPÇÃO INVÁLIDA.");
+            }
+
+        } while (true);
+
+    }
+    
+    /*
+    Créditos especiais para Ftsuda82
+    https://github.com/ftsuda82/IntegracaoJBDC/blob/master/src/main/java/br/senac/tads/pi3/ftsuda/integracaojdbc/ContatosApp.java
+    */
 }
